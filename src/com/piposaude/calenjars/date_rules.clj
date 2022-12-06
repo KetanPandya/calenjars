56ad059b553d9e1e1a2e65b95832747aaa311887
mn-dimension <thecontinium+mn.dimension@outlook.com>
2022-12-06 17:32:58 +0000

    add 4THU date rule for thanks gving

--- src/com/piposaude/calenjars/date_rules.clj
+++ src/com/piposaude/calenjars/date_rules.clj
@@ -18,7 +18,31 @@
   (let [p (previous date non-business-days)]
   (if (same-month? date p) p (following date non-business-days))))
 
+(defn- previous-months
+  ([d n] (let [pd (t/- d (t/new-period n :months))] {:year (t/int (t/year pd)) :month (t/int (t/month pd))}))
+  ([d] (previous-months d 1)))
+
+(defn- day-of-month [d n]
+  (t/new-date (t/int (t/year d)) (t/int (t/month d)) n))
+
+(defn- first-of-month [d] (day-of-month d 1))
+
+(defn- yesterday [d]
+  (relative-date-add d -1 :days []))
+
+(defn- end-of-last-month [d] (yesterday (first-of-month d)))
+
+(defn- next-day [d day]
+  (loop [d (relative-date-add d 1 :days [])] (if (= (t/day-of-week d) day) d (recur (relative-date-add d 1 :days [])))))
+
+(defn- nth-day-in-month [d day n]
+  (loop [d (end-of-last-month d) n n] (if (= n 0) d (recur (next-day d day) (dec n)))))
+
+(defn- fourth-thursday [d] (nth-day-in-month d t/THURSDAY  4))
+
 (defmulti date-rule (fn [rule _ _] rule))
+
+(defmethod date-rule "4THU" [_ date non-business-days] (fourth-thursday date ))
 (defmethod date-rule "F" [_ date non-business-days] (following date non-business-days))
 (defmethod date-rule "P" [_ date non-business-days] (previous date non-business-days))
 (defmethod date-rule "MF" [_ date non-business-days] (modified-following date non-business-days))
